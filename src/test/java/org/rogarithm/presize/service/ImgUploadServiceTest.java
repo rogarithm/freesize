1c44d1eafa64862c65aa89a87ebbb68986d5bbdc
sehoon.gim <sehoongim@gmail.com>
2025-01-02 23:17:03 +0900

    테스트 정리한다

--- src/test/java/org/rogarithm/presize/service/ImgUploadServiceTest.java
+++ src/test/java/org/rogarithm/presize/service/ImgUploadServiceTest.java
@@ -3,14 +3,11 @@ package org.rogarithm.presize.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ImgUploadServiceTest {
     @Test
-    public void t() {
+    public void test_parsing_DataBufferLimitException_err_msg() {
         String errorMsg = "Exceeded limit on max bytes to buffer : 2097152";
         String problematicSize = errorMsg.split(":")[1].replaceAll(" ", "");
         Assertions.assertThat(Integer.parseInt(problematicSize)).isEqualTo(2097152);
     }
-
 }
