5452e519f6517db51cec641ac75c30812d295c81
sehoon.gim <sehoongim@gmail.com>
2025-01-09 20:16:35 +0900

    쓰지 않는 API를 삭제한다
     - 테스트용으로 만들었던 API를 지운다

--- src/main/java/org/rogarithm/presize/web/ImgUploadController.java
+++ src/main/java/org/rogarithm/presize/web/ImgUploadController.java
@@ -6,23 +6,16 @@ import org.rogarithm.presize.web.request.ImgUploadRequest;
 import org.rogarithm.presize.web.response.ImgUploadResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
-import java.io.IOException;
 
 @Controller
 @RequestMapping
 public class ImgUploadController {
     private static final Logger log = LoggerFactory.getLogger(ImgUploadController.class);
-
-    @Value("${file.dir}")
-    private String fileDir;
 
     private final ImgUploadService service;
 
@@ -40,22 +33,6 @@ public class ImgUploadController {
     public String showResult(@ModelAttribute("resizedImg") String resizedImg, Model model) {
         model.addAttribute("resizedImg", resizedImg);
         return "result-img";
-    }
-
-    @PostMapping("/upload-file")
-    public String saveFile(@RequestBody ImgUploadRequest request) throws IOException {
-
-        service.uploadImg(ImgUploadDto.from(request));
-        MultipartFile file = request.getFile();
-        log.info("multipartFile={}", file);
-
-        if (!file.isEmpty()) {
-            String fullPath = fileDir + File.separator + file.getOriginalFilename();
-            log.info("파일 저장 fullPath={}", fullPath);
-            file.transferTo(new File(fullPath));
-        }
-
-        return "upload-form";
     }
 
     @PostMapping("/upload")
