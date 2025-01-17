d99f26c463f49a70bacbd7ec47971bc494a8e39a
sehoon gim <sehoongim@gmail.com>
2025-01-17 16:17:11 +0900

    프론트와 연결 가능하도록 api를 수정 및 분리한다 (#8)
    * upscale, uncrop AI model 변경 사항을 반영한다
    
     - AI model에 요청 시 필요한 파라미터 목록이 바뀌었다
     - 기존 api에 변경 사항을 반영한다
    
    * 목적에 따라 api를 분리한다
    
     - 기존 upscale, uncrop post api를 프론트에서 호출할 수 있도록 응답값을
       json으로 만들어 넘기도록 고친다
     - 기존에 테스트 목적으로 만들었던 api(프론트용으로 바꾸기 전 api
       포함해서)는 /test 하위 url로 옮겨서 계속 쓸 수 있도록 한다
    
    * swagger 문서 생성한다

--- src/main/java/org/rogarithm/presize/service/dto/ImgUncropDto.java
+++ src/main/java/org/rogarithm/presize/service/dto/ImgUncropDto.java
@@ -1,5 +1,6 @@
 package org.rogarithm.presize.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.rogarithm.presize.web.request.ImgUncropRequest;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,20 +8,27 @@ import java.util.Base64;
 
 public class ImgUncropDto {
     private String img;
+    @JsonProperty("target_ratio")
+    private String targetRatio;
 
-    public ImgUncropDto(MultipartFile file) {
+    public ImgUncropDto(MultipartFile file, String targetRatio) {
         try {
             this.img = Base64.getEncoder().encodeToString(file.getBytes());
         } catch (Exception e) {
             throw new RuntimeException("Failed to encode image", e);
         }
+        this.targetRatio = targetRatio;
     }
 
     public static ImgUncropDto from(ImgUncropRequest request) {
-        return new ImgUncropDto(request.getFile());
+        return new ImgUncropDto(request.getFile(), request.getTargetRatio());
     }
 
     public String getImg() {
         return img;
+    }
+
+    public String getTargetRatio() {
+        return targetRatio;
     }
 }
--- src/main/java/org/rogarithm/presize/web/request/ImgUpscaleRequest.java
+++ src/main/java/org/rogarithm/presize/web/request/ImgUpscaleRequest.java
@@ -4,39 +4,29 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ImgUpscaleRequest {
     private MultipartFile file;
-    private String height;
-    private String width;
+    private String upscaleRatio;
 
     public ImgUpscaleRequest() {
     }
 
-    public ImgUpscaleRequest(MultipartFile file, String height, String width) {
+    public ImgUpscaleRequest(MultipartFile file, String upscaleRatio) {
         this.file = file;
-        this.height = height;
-        this.width = width;
+        this.upscaleRatio = upscaleRatio;
     }
 
     public MultipartFile getFile() {
         return file;
     }
 
-    public String getHeight() {
-        return height;
-    }
-
-    public String getWidth() {
-        return width;
+    public String getUpscaleRatio() {
+        return upscaleRatio;
     }
 
     public void setFile(MultipartFile file) {
         this.file = file;
     }
 
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public void setWidth(String width) {
-        this.width = width;
+    public void setUpscaleRatio(String upscaleRatio) {
+        this.upscaleRatio = upscaleRatio;
     }
 }
