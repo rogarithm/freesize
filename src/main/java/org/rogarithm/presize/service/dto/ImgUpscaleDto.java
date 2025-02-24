dff0bb1455ed50ba9173a3124661485a236685a7
sehoon.gim <sehoongim@gmail.com>
2025-02-24 23:20:32 +0900

    dto 생성 실패 시 로그 출력한다

--- src/main/java/org/rogarithm/presize/service/dto/ImgUpscaleDto.java
+++ src/main/java/org/rogarithm/presize/service/dto/ImgUpscaleDto.java
@@ -3,11 +3,15 @@ package org.rogarithm.presize.service.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.rogarithm.presize.service.TempFileStoreManager;
 import org.rogarithm.presize.web.request.ImgUpscaleRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
 
 public class ImgUpscaleDto {
+    private static final Logger log = LoggerFactory.getLogger(ImgUpscaleDto.class);
+
     private String img;
     @JsonProperty("upscale_ratio")
     private String upscaleRatio;
@@ -25,6 +29,7 @@ public class ImgUpscaleDto {
             // Base64 인코딩
             this.img = encodeWithPadding(Base64.getEncoder().encodeToString(fileBytes));
         } catch (Exception e) {
+            log.error(e.getMessage(), e);
             throw new RuntimeException("ImgUpscaleDto: Failed to encode image", e);
         }
         this.upscaleRatio = upscaleRatio;
