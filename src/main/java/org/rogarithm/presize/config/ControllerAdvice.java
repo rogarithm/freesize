f259308e421c189591196e207200df519fd7b926
sehoon.gim <sehoongim@gmail.com>
2025-03-02 21:34:24 +0900

    예외 처리기에 등록되지 않은 예외를 등록한다

--- src/main/java/org/rogarithm/presize/config/ControllerAdvice.java
+++ src/main/java/org/rogarithm/presize/config/ControllerAdvice.java
@@ -1,6 +1,6 @@
 package org.rogarithm.presize.config;
 
-import org.rogarithm.presize.exception.AiModelRequestFailException;
+import org.rogarithm.presize.exception.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +16,30 @@ public class ControllerAdvice {
 
     @ExceptionHandler(AiModelRequestFailException.class)
     protected ResponseEntity<ErrorResponse> handleAiModelRequestFailException(final AiModelRequestFailException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+        return errorResponse.makeResponseEntity();
+    }
+
+    @ExceptionHandler(AiModelRequestEncodingFailException.class)
+    protected ResponseEntity<ErrorResponse> handleAiModelRequestEncodingFailException(final AiModelRequestEncodingFailException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+        return errorResponse.makeResponseEntity();
+    }
+
+    @ExceptionHandler(AiModelRequestMakingFailException.class)
+    protected ResponseEntity<ErrorResponse> handleAiModelRequestMakingFailException(final AiModelRequestMakingFailException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+        return errorResponse.makeResponseEntity();
+    }
+
+    @ExceptionHandler(S3FileUploadFailException.class)
+    protected ResponseEntity<ErrorResponse> handleS3FileUploadFailException(final S3FileUploadFailException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+        return errorResponse.makeResponseEntity();
+    }
+
+    @ExceptionHandler(StoreTempFileFailException.class)
+    protected ResponseEntity<ErrorResponse> handleStoreTempFileFailException(final StoreTempFileFailException exception) {
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
         return errorResponse.makeResponseEntity();
     }
